98756cba4e60b63e4c2b50f987be7a1538f411b2
mykhailoleskiv <daniskort@ukr.net>
2019-02-09 13:11:14 +0200

    Made lyrics autosetting

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
 
+from lyrics import get_songs_data
 
 PLAYLIST_LINK = 'https://www.youtube.com/playlist?list=PLQzNb__3Hyqq_gqsnUOUvGLSSgw2IgQ7y'
 OUTPUT_PATH = '/home/mykhailo/repos/ydow/music/'
+# youtube-dl names files with weird symbols in the beginning
 END_OF_SONG_NAME = -16
 
 
@@ -22,3 +24,4 @@ def rename_files(end_of_song_name, path=None):
 if __name__ == '__main__':
     download_by_url(PLAYLIST_LINK, OUTPUT_PATH)
     rename_files(END_OF_SONG_NAME, OUTPUT_PATH)
+    get_songs_data(OUTPUT_PATH)
--- lyrics.py
+++ lyrics.py
@@ -4,8 +4,6 @@ import re
 import eyed3
 from PyLyrics import PyLyrics
 
-from index import OUTPUT_PATH
-
 
 def parse_song_name(song_name):
     end = song_name.find('-')
@@ -13,18 +11,20 @@ def parse_song_name(song_name):
         end = song_name.find('—')
     artist = song_name[:end]
     artist = re.sub("[\(\[].*?[\)\]]", "", artist)
-    song = re.sub("[\(\[].*?[\)\]]", "", song_name[end+1:-4])
+    song = re.sub("[\(\[].*?[\)\]]", "", song_name[end + 1:-4])
     return [artist.strip(), song.strip()]
 
 
-for filename in os.listdir(OUTPUT_PATH):
-    data = parse_song_name(filename)
-    # try:
-    #     lyrics = PyLyrics.getLyrics(data[0], data[1])
-    # except ValueError:
-    #     print('Song or Singer does not exist or the API does not have Lyrics')
-    track = eyed3.load(OUTPUT_PATH + filename)
-    track.tag.artist = data[0]
-    track.tag.title = data[1]
-    # TODO: change set audio lyrics
-    track.tag.save()
+def get_songs_data(OUTPUT_PATH):
+    for filename in os.listdir(OUTPUT_PATH):
+        data = parse_song_name(filename)
+        track = eyed3.load(OUTPUT_PATH + filename)
+        track.tag.artist = data[0]
+        track.tag.title = data[1]
+        try:
+            lyrics = PyLyrics.getLyrics(data[0].title(), data[1].title())
+        except ValueError:
+            print('{}\'s lyrics do not exists'.format(data[0].title() + ' - ' + data[1].title()))
+        else:
+            track.tag.lyrics.set(lyrics)
+        track.tag.save()
